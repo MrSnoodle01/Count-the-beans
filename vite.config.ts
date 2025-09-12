1192568e8a16ed450be22885cf38015fbb04fbf5
Josh Richardson <richardsonjosh03@gmail.com>
2025-09-12 08:28:47 -0700

    removed terminal package

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import terminal from 'vite-plugin-terminal'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   loadEnv(mode, process.cwd(), "VITE_")
   return {
     base: '/Count-the-beans/',
-    plugins: [
-      react(),
-      terminal({ console: 'terminal', output: ['terminal', 'console'] })
-    ],
+    plugins: [react()],
   }
 })
